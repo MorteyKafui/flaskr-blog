b1ce3f1714440a189adf75d5d56b71864aaeaf97
Kafui Mortey <morteykafui@gmail.com>
2024-08-09 22:08:45 +0000

    add blog blueprints

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -19,10 +19,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route("/")
-    def index():
-        return "hello"
-
     from . import db
 
     db.init_app(app)
@@ -30,5 +26,10 @@ def create_app(test_config=None):
     from . import auth
 
     app.register_blueprint(auth.bp)
+
+    from . import blog
+
+    app.register_blueprint(blog.bp)
+    app.add_url_rule("/", endpoint="index")
 
     return app
